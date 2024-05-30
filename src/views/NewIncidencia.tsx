9874714284eb2a9c28347942ac05beaa0da74ffc
MiloDeSkorpio <arcadio.2es0@gmail.com>
2024-05-30 13:54:41 -0600

    BasicNavigationWithLink

--- src/views/NewIncidencia.tsx
+++ src/views/NewIncidencia.tsx
@@ -1,9 +1,17 @@
 
-
+import { Link } from "react-router-dom"
 export default function NewIncidencia() {
   return (
-    <div>
-      NewIncidencia
-    </div>
+    <>
+      <div className="flex justify-between">
+        <h2 className="text-4xl font-black text-slate-500">Registrar Incidencias</h2>
+        <Link 
+          to="/"
+          className="rounded-ms bg-indigo-600 p-3 text-sm font-bold text-white shadow-sm hover:bg-indigo-500"
+        >
+        Incidencias
+        </Link>
+      </div>
+    </>
   )
 }
