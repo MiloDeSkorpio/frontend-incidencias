80d820bff2db5ec192553e6e442f2d38d6164c5a
MiloDeSkorpio <arcadio.2es0@gmail.com>
2024-06-10 13:38:04 -0600

    TailwidConfInputNumber

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,19 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function({ addBase }) {
+      addBase({
+        'input[type="number"]::-webkit-inner-spin-button, input[type="number"]::-webkit-outer-spin-button': {
+          '-webkit-appearance': 'none',
+          margin: '0',
+        },
+        'input[type="number"]': {
+          '-moz-appearance': 'textfield',
+        },
+      })
+    },
+  ],
 };
+
 
