d5f6a28ff8b244e054f119da06d3b1563596cd2e
MiloDeSkorpio <arcadio.2es0@gmail.com>
2024-06-10 14:02:44 -0600

    UpdatedWithInput

--- src/views/ResetPass.tsx
+++ src/views/ResetPass.tsx
@@ -1,4 +1,4 @@
-import InputEmail from "./cruds/helpers/InputEmail";
+import Input from "./cruds/helpers/Input";
 import InputSubmit from "./cruds/helpers/InputSubmit";
 import MiniButton from "./cruds/helpers/MiniButton";
 
@@ -7,6 +7,10 @@ export default function ResetPass() {
   const btnTit = 'Enviar Solicitud';
   const hrefMin = '/'
   const miniTit = 'Inicio de Sesión'
+  // datos input email
+  const lblEma = 'Email:'
+  const kwEma = 'email'
+  const namePhEma = 'pema18@correo.com'
   return (
     <>
     <div className="flex justify-between">
@@ -15,11 +19,17 @@ export default function ResetPass() {
     <form
       className="mt-5"
     >
-
-    <InputEmail />
+      <Input
+        type={kwEma}
+        lblName={lblEma}
+        hmtlFor={kwEma}
+        id={kwEma}
+        name={kwEma}
+        placeholder={namePhEma}
+      />
     <InputSubmit  value={btnTit}/>
     <MiniButton href={hrefMin} namebtn={miniTit}/>
     </form>
   </>
   )
-}
+} 
