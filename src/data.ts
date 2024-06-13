82e0f95b0b1b587e50b33d2fe95579755423fbfb
Milito de Skorpio <arcadio.2es0@gmail.com>
2024-06-12 20:03:36 -0600

    DataFile

---
+++ src/data.ts
@@ -0,0 +1,43 @@
+export const  registros = {
+    incidencias: [
+        {
+          id: 1,
+          idBus: 1,
+          idFalla: 2,
+          detalles: 'El Validador tarda mucho en leer tarjetas o no las lee',
+          idStatus: 1,
+          idSuper: 2,
+          idTec: 4,
+          assignedAt:' 2024-05-28T20:01:24.494Z',
+          observaciones: 'El validador contaba con un problema en el lector del SAM',
+          validatedAt: '2024-05-28T20:01:24.494Z',
+          timeAtention: 6.7
+        }, 
+        {
+          id: 3,
+          idBus: 2,
+          idFalla: 8,
+          detalles: 'La pantalla esta completamente pasmada',
+          idStatus: 1,
+          idSuper: 2,
+          idTec: 4,
+          assignedAt:' 2024-05-28T20:01:24.494Z',
+          observaciones: 'El validador contaba con un problema en el lector del SAM',
+          validatedAt: '2024-05-28T20:01:24.494Z',
+          timeAtention: 6.7
+        }, 
+        {
+          id: 7,
+          idBus: 1,
+          idFalla: 12,
+          detalles: 'El Validador Sin audio',
+          idStatus: 1,
+          idSuper: 2,
+          idTec: 4,
+          assignedAt:' 2024-05-28T20:01:24.494Z',
+          observaciones: 'El validador contaba con un problema de voltaje',
+          validatedAt: '2024-05-28T20:01:24.494Z',
+          timeAtention: 6.7
+        } 
+    ]
+}
